edac85551086160ecdf40e4b45d1aeae4e8720a0
Alex Moore-Niemi <moore.niemi@gmail.com>
2015-11-03 20:01:55 -0500

    setting up failing test, console

--- spec/amsa_mason_spec.rb
+++ spec/amsa_mason_spec.rb
@@ -7,9 +7,18 @@ describe AmsaMason do
 end
 
 describe AmsaMason do
+  let(:parsed_mason) do
+    JSON.parse(File.read('spec/support/mason.json'))
+  end
   let(:model) { AmsaMason::ResourceModel.new(title: "Title") }
   let(:serializer) { AmsaMason::ResourceSerializer.new(model) }
+
   it 'defines an Adapter' do
     expect(AmsaMason::Adapter.new(serializer)).to be_a AmsaMason::Adapter
+  end
+
+  it 'serializes Mason correctly' do
+    adapter = AmsaMason::Adapter.new(serializer)
+    expect(JSON.parse(adapter.as_json)).to eq(parsed_mason)
   end
 end
